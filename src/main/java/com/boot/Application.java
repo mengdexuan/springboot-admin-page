fa3c85bd13cc1b2948b1ed3f80c627ef41ed213d
mengdexuan <mengdexuan@iciyun.com>
2020-02-08 11:40:40 +0800

    定时删除请求记录

--- src/main/java/com/boot/Application.java
+++ src/main/java/com/boot/Application.java
@@ -2,8 +2,9 @@ package com.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class Application {
 
