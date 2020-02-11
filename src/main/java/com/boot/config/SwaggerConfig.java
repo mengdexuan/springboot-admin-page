6615897a9ba7817a36b041eb4e376e8cdb53da80
mengdexuan <mengdexuan@iciyun.com>
2020-02-11 20:04:23 +0800

    引入 knife4j ，增强 swagger ui 显示

--- src/main/java/com/boot/config/SwaggerConfig.java
+++ src/main/java/com/boot/config/SwaggerConfig.java
@@ -1,34 +1,56 @@
 package com.boot.config;
 
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author mengdexuan on 2019/10/23 18:41.
  */
 @Configuration
 @EnableSwagger2
+@EnableKnife4j
+@Import(BeanValidatorPluginsConfiguration.class)
 public class SwaggerConfig {
 
 
 	@Bean
 	public Docket api() {
+
+		ParameterBuilder ticketPar = new ParameterBuilder();
+		List<Parameter> pars = new ArrayList<Parameter>();
+		ticketPar.name("loginKey").description("用户登录key")
+				.modelRef(new ModelRef("string")).parameterType("header")
+				.required(false).build(); //header中的ticket参数非必填，传空也可以
+		pars.add(ticketPar.build());    //根据每个方法名也知道当前方法在设置什么参数
+
+
 		return new Docket(DocumentationType.SWAGGER_2)
 				.apiInfo(apiInfo())
 				.select()
 				// 自行修改为自己的包路径
 				.apis(RequestHandlerSelectors.basePackage("com.boot"))
 				.paths(PathSelectors.any())
-				.build();
+				.build().globalOperationParameters(pars);
+
 	}
+
 
 	private ApiInfo apiInfo() {
 		return new ApiInfoBuilder()
