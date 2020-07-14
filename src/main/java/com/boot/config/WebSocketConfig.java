d2f47665270cd80baf55b764e9d62a26c7bfd546
mengdexuan <mengdexuan@iciyun.com>
2020-07-14 14:12:49 +0800

    类结构调整

--- src/main/java/com/boot/config/WebSocketConfig.java
+++ src/main/java/com/boot/config/WebSocketConfig.java
@@ -1,10 +1,12 @@
 package com.boot.config;
 
+import com.boot.biz.webshell.ssh.SshShellHandler;
+import com.boot.biz.wstest.EchoHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 /**
  * @author mengdexuan on 2020/4/1 13:21.
@@ -12,7 +14,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocket
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer ,WebSocketConfigurer {
 
 	public static String sysTime = "/sysTime";
 	public static String sysInfoLog = "/sysInfoLog";
@@ -39,5 +41,24 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	}
 
 
+	@Autowired
+	EchoHandler echoHandler;
+	@Autowired
+	SshShellHandler sshShellHandler;
+
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
+
+		// withSockJS 声明启用支持 sockJS
+		webSocketHandlerRegistry.addHandler(echoHandler, "/echo").withSockJS();
+
+		webSocketHandlerRegistry.addHandler(sshShellHandler, "/sshShellHandler").setAllowedOrigins("*").withSockJS();
+	}
+
+
+	@Bean
+	public ServerEndpointExporter serverEndpointExporter() {
+		return new ServerEndpointExporter();
+	}
 
 }
