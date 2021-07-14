bd4d9f8ebb17cd12f0017f231d2637233a2fa6b7
mengdexuan <mengdexuan@iciyun.com>
2021-07-14 14:27:26 +0800

    jobutil 修改

--- src/main/java/com/boot/base/job/util/JobUtil.java
+++ src/main/java/com/boot/base/job/util/JobUtil.java
@@ -1,5 +1,7 @@
 package com.boot.base.job.util;
 
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.date.DateUtil;
 import com.boot.base.job.entity.Job;
 import com.boot.base.job.runner.JobRunnable;
 import com.boot.base.job.service.JobService;
@@ -7,11 +9,11 @@ import com.google.common.collect.Maps;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.support.CronTrigger;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
 import java.util.Map;
 import java.util.concurrent.ScheduledFuture;
 
@@ -82,6 +84,27 @@ public class JobUtil {
             future.cancel(true);
             cache.remove(id);
         }
+    }
+
+
+    /**
+     * 以当前时间为依据，判断给定的 date 是否过了 seconds 秒，如果过了 返回 true，否则返回 false
+     * @param date
+     * @param seconds
+     */
+    public static boolean isPassSeconds(Date date, int seconds){
+
+        Date now = new Date();
+        DateTime date2 = DateUtil.offsetSecond(date, seconds);
+
+//        比较结果，如果date1 &lt; date2，返回数小于0，date1==date2返回0，date1 &gt; date2 大于0
+        int result = DateUtil.compare(now,date2);
+        if (result>0){
+            //当前时间大
+            return true;
+        }
+
+        return false;
     }
 
 
