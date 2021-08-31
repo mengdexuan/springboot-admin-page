e90a28ac6401684a0cd988ebcffa8efb60234a77
mengdexuan <mengdexuan@iciyun.com>
2021-08-31 11:27:21 +0800

    update

--- src/main/java/com/boot/biz/dict/controller/DictController.java
+++ src/main/java/com/boot/biz/dict/controller/DictController.java
@@ -1,13 +1,13 @@
 package com.boot.biz.dict.controller;
 
 
+import cn.hutool.core.bean.BeanUtil;
 import com.boot.base.Result;
 import com.boot.base.ResultUtil;
 import com.boot.base.util.SpringContextUtil;
 import com.boot.biz.dict.entity.Dict;
 import com.boot.biz.dict.service.DictService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
@@ -25,77 +25,100 @@ import java.util.List;
 @RequestMapping("/dict")
 public class DictController {
 
-        @Autowired
-        private DictService dictService;
+	@Autowired
+	private DictService dictService;
 
-        /**
-         * 获取数据字典表列表
-         */
-        @GetMapping(value = "/list")
-        public Result<List<Dict>> list() {
-            return ResultUtil.buildSuccess(dictService.findAll(Sort.by(Sort.Order.asc("orderNo"))));
-        }
+	/**
+	 * 获取数据字典表列表
+	 */
+	@GetMapping(value = "/list")
+	public Result<List<Dict>> list() {
+		return ResultUtil.buildSuccess(dictService.findAll());
+	}
 
-        /**
-         * 新增数据字典表
-         */
-        @PostMapping(value = "/add")
-        public Result<String> add(Dict dataCentralDict) {
-            dataCentralDict.setOrderNo(1);
-            dataCentralDict.setEnable(1);
-            dataCentralDict.setCreateTime(new Date());
+	/**
+	 * 新增数据字典表
+	 */
+	@PostMapping(value = "/add")
+	public Result<String> add(Dict dataCentralDict) {
+		dataCentralDict.setOrderNo(1);
+		dataCentralDict.setEnable(1);
+		dataCentralDict.setCreateTime(new Date());
 
-            dictService.save(dataCentralDict);
+		dictService.save(dataCentralDict);
 
-            dictService.reloadDictCache();
+		dictService.reloadDictCache();
 
-            return ResultUtil.buildSuccess();
-        }
+		return ResultUtil.buildSuccess();
+	}
 
-        /**
-         * 删除数据字典表
-         */
-        @PostMapping(value = "/delete/{id}")
-        public Result<String> delete(@PathVariable("id") Long id) {
-            dictService.delete(id);
-            dictService.reloadDictCache();
-            return ResultUtil.buildSuccess();
-        }
-
-        /**
-         * 修改数据字典表
-         */
-        @PostMapping(value = "/update")
-        public Result<String> update(Dict dataCentralDict) {
-
-            Dict entity = dictService.getByFieldEqual("dictKey", dataCentralDict.getDictKey());
-
-            boolean changeVal = true;
-
-            if (entity.getDictValue().equals(dataCentralDict.getDictValue())){
-                // val 值没有被修改
-                changeVal = false;
-            }
-
-            dictService.update(dataCentralDict);
-
-            dictService.reloadDictCache();
-
-            if (changeVal){
-                //发布数据字典更新事件
-                SpringContextUtil.applicationContext.publishEvent(dataCentralDict);
-            }
+	/**
+	 * 删除数据字典表
+	 */
+	@PostMapping(value = "/delete/{id}")
+	public Result<String> delete(@PathVariable("id") Long id) {
+		dictService.delete(id);
+		dictService.reloadDictCache();
+		return ResultUtil.buildSuccess();
+	}
 
 
-            return ResultUtil.buildSuccess();
-        }
+	/**
+	 * 复制1条配置
+	 */
+	@PostMapping(value = "/copy/{id}")
+	public Result<String> copy(@PathVariable("id") Long id) {
 
-        /**
-         * 获取数据字典表
-         */
-        @GetMapping(value = "/get/{id}")
-        public Result<Dict> get(@PathVariable("id") Long id) {
-            return ResultUtil.buildSuccess(dictService.get(id));
-        }
+		Dict one = dictService.get(id);
+
+		Dict dict = new Dict();
+
+		BeanUtil.copyProperties(one, dict);
+
+		dict.setDictKey(dict.getDictKey() + "_copy");
+		dict.setId(null);
+
+		dictService.save(dict);
+
+		dictService.reloadDictCache();
+		return ResultUtil.buildSuccess();
+	}
+
+
+	/**
+	 * 修改数据字典表
+	 */
+	@PostMapping(value = "/update")
+	public Result<String> update(Dict dataCentralDict) {
+
+		Dict entity = dictService.getByFieldEqual("dictKey", dataCentralDict.getDictKey());
+
+		boolean changeVal = true;
+
+		if (entity.getDictValue().equals(dataCentralDict.getDictValue())) {
+			// val 值没有被修改
+			changeVal = false;
+		}
+
+		dictService.update(dataCentralDict);
+
+		dictService.reloadDictCache();
+
+		if (changeVal) {
+			//发布数据字典更新事件
+			SpringContextUtil.applicationContext.publishEvent(dataCentralDict);
+		}
+
+
+		return ResultUtil.buildSuccess();
+	}
+
+	/**
+	 * 获取数据字典表
+	 */
+	@GetMapping(value = "/get/{id}")
+	public Result<Dict> get(@PathVariable("id") Long id) {
+		return ResultUtil.buildSuccess(dictService.get(id));
+	}
 }
 
